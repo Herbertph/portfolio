315d09811dcfa3c6b9de4a522135f7097f1c8a4c
Herbert Parasio <herbert.parasio@gmail.com>
2025-01-14 22:09:58 -0400

    Started Main Page

---
+++ react-portfolio/src/components/Hero/Hero.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styles from "./Hero.module.css";
+import { getImageUrl } from '../../utils';
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+    <div className={styles.content}>
+      <h1 className={styles.title}>Herbert Parasio - Full Stack Developer</h1>
+      <p className={styles.description}>
+        5+ Years of proven experience working with databases including PL/SQL, SQL Server, MySQL, MongoDB and PostgreSQL
+        2+ Years working with JavaScript and its main frameworks React, Angular and NodeJS
+        2+ Years of Object-Oriented Programming (OOP) experience with Java (Spring Framework) and C# (Asp.net)
+    </p>
+    <a href="mailto:herbert.parasio@gmail.com" className={styles.contactBtn}> Contact Me</a>
+    </div>
+    <img src={getImageUrl('hero/Hero.png')} alt="Hero" className={styles.heroImg} />
+    <div className={StyleSheet.topBlur}/>
+    <div className={StyleSheet.bottomBlur}/>
+    </section>
+  );
+};
