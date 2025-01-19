af3957e937c6d3bcb147f04c7bea241dd9623c83
Herbert Parasio <herbert.parasio@gmail.com>
2025-01-18 20:49:48 -0400

    Adjustment on main section

--- react-portfolio/src/components/Hero/Hero.jsx
+++ react-portfolio/src/components/Hero/Hero.jsx
@@ -8,9 +8,7 @@ export const Hero = () => {
     <div className={styles.content}>
       <h1 className={styles.title}>Herbert Parasio - Full Stack Developer</h1>
       <p className={styles.description}>
-        5+ Years of proven experience working with databases including PL/SQL, SQL Server, MySQL, MongoDB and PostgreSQL<br/>
-        2+ Years working with JavaScript and its main frameworks React, Angular and NodeJS<br/> 
-        2+ Years of Object-Oriented Programming (OOP) experience with Java (Spring Boot)
+        Full Stack developer with strong background working with Databases, Javascript and Java!
     </p>
     <a href="mailto:herbert.parasio@gmail.com" className={styles.contactBtn}> Contact Me</a>
     </div>
