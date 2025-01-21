28452a4788c831c53778410dd904656bf73b2654
Herbert Parasio <herbert.parasio@gmail.com>
2025-01-20 22:41:01 -0400

    minor changes to about section

--- react-portfolio/src/components/About/About.jsx
+++ react-portfolio/src/components/About/About.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
 
 export const About = () => {
-    return <div>About</div>;
+    return 
+    {
+        <section>
+            <h2>About</h2>
+            <div><img src="" alt="" />
+            <ul><li><img src="" alt="" /></li></ul>
+            </div>
+        </section>
+    }
 };
