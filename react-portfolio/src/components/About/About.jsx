b3b10f6a65fb4242a7d1ec73526b467edd754181
Herbert Parasio <herbert.parasio@gmail.com>
2025-01-24 23:33:21 -0400

    Upadate classes

--- react-portfolio/src/components/About/About.jsx
+++ react-portfolio/src/components/About/About.jsx
@@ -5,28 +5,28 @@ import styles from "./About.module.css";
 export const About = () => {
     return (
         <section className={styles.container}>
-            <h2>About</h2>
-            <div>
-                <img src={getImageUrl("about/aboutImage.png")} alt="Me sitting with a laptop" />
-                <ul>
-                    <li>
-                        <img src={getImageUrl("about/cursorIcon.png")} alt="arrow icon" />
-                        <div>
+            <h2 className={styles.title}>About</h2>
+            <div className={styles.content}>
+                <img src={getImageUrl("about/aboutImage.png")} alt="Me sitting with a laptop" className={styles.aboutImage}/>
+                <ul className={styles.aboutItems}>
+                    <li className={styles.aboutItem}>
+                        <img src={getImageUrl("about/cursorIcon.png")} alt="arrow icon"/>
+                        <div className={styles.aboutItemText}>
                             <h1>Frontend Developer</h1>
                             <p>2+ Years working with JavaScript and its main frameworks React, Angular and NodeJS </p>
                         </div>
                     </li>
-                    <li>
+                    <li className={styles.aboutItem}>
                         <img src={getImageUrl("about/serverIcon.png")} alt="arrow icon" />
-                        <div>
+                        <div className={styles.aboutItemText}>
                             <h1>Backend Developer</h1>
                             <p>2+ Years of Object-Oriented Programming (OOP) experience with Java (Spring Framework) and C# (Asp.net)</p>
                         </div>
                     </li>
                     
-                    <li>
+                    <li className={styles.aboutItem}>
                         <img src={getImageUrl("about/uiIcon.png")} alt="arrow icon" />
-                        <div>
+                        <div className={styles.aboutItemText}>
                             <h1>Database Developer</h1>
                             <p>5+ Years of proven experience working with databases including PL/SQL, SQL Server, MySQL, MongoDB and PostgreSQL</p>
                         </div>
