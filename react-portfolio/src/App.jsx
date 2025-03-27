c861525cbceab27c293ceb0044c9686bdbcca0f0
Herbert Parasio <91493748+Herbertph@users.noreply.github.com>
2025-03-27 12:20:35 -0300

    Contact and Projects Updated

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -3,7 +3,8 @@ import { Navbar } from './components/Navbar/Navbar'
 import { Hero } from './components/Hero/Hero'
 import { About } from './components/About/About'
 import { Experience } from './components/Experience/Experience'
-
+import { Projects } from './components/Projects/Projects'
+import { Contact } from './components/Contact/Contact'
 function App() {
   return (
   <div className={styles.App}>
@@ -11,6 +12,8 @@ function App() {
   <Hero/>
   <About/>
   <Experience/>
+  <Projects/>
+  <Contact/>
   </div>
   )
 }
