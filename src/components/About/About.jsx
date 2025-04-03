b644e46dd2218347aaab518efdebb35eef4a5244
Herbert Parasio <91493748+Herbertph@users.noreply.github.com>
2025-04-03 10:12:17 -0300

    Update to deploy

---
+++ src/components/About/About.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { getImageUrl } from '../../utils';
+import styles from "./About.module.css";
+
+export const About = () => {
+    return (
+        <section className={styles.container} id="about">
+            <h2 className={styles.title}>About</h2>
+            <div className={styles.content}>
+                <img src={getImageUrl("about/aboutImage.png")} alt="Me sitting with a laptop" className={styles.aboutImage}/>
+                <ul className={styles.aboutItems}>
+                    <li className={styles.aboutItem}>
+                        <img src={getImageUrl("about/cursorIcon.png")} alt="arrow icon"/>
+                        <div className={styles.aboutItemText}>
+                            <h1>Frontend Developer</h1>
+                            <p>2+ Years working with JavaScript and its main frameworks React, Angular and NodeJS </p>
+                        </div>
+                    </li>
+                    <li className={styles.aboutItem}>
+                        <img src={getImageUrl("about/serverIcon.png")} alt="arrow icon" />
+                        <div className={styles.aboutItemText}>
+                            <h1>Backend Developer</h1>
+                            <p>2+ Years of Object-Oriented Programming (OOP) experience with Java (Spring Framework) and C# (Asp.net)</p>
+                        </div>
+                    </li>
+                    
+                    <li className={styles.aboutItem}>
+                        <img src={getImageUrl("about/uiIcon.png")} alt="arrow icon" />
+                        <div className={styles.aboutItemText}>
+                            <h1>Database Developer</h1>
+                            <p>5+ Years of proven experience working with databases including PL/SQL, SQL Server, MySQL, MongoDB and PostgreSQL</p>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </section>
+    );
+};
+
+export default About;
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from "./Hero.module.css";
+import { getImageUrl } from '../../utils';
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+    <div className={styles.content}>
+      <h1 className={styles.title}>Herbert Parasio - Full Stack Developer</h1>
+      <p className={styles.description}>
+        Full Stack developer with strong background working with Databases, Javascript and Java!
+    </p>
+    <a href="mailto:herbert.parasio@gmail.com" className={styles.contactBtn}> Contact Me</a>
+    </div>
+    <img src={getImageUrl('hero/Hero.png')} alt="Hero" className={styles.heroImg} />
+    <div className={StyleSheet.topBlur}/>
+    <div className={StyleSheet.bottomBlur}/>
+    </section>
+  );
+};
